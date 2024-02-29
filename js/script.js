3eb99440509225852e0deb6ab84262c29973c347
daffaya <daffapasya21@gmail.com>
2024-02-29 18:30:55 +0700

    configure js for message us in aside and banner section.

--- js/script.js
+++ js/script.js
@@ -4,3 +4,37 @@ fetch("navbar.html")
     document.getElementById("navbar-container").innerHTML = html;
   })
   .catch((error) => console.error("Error fetching navbar:", error));
+
+document
+  .getElementById("data-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const timeStamp = new Date();
+
+    const receivedData = document.createElement("div");
+    receivedData.innerHTML = `
+  <p><strong>Current Time: </strong> ${timeStamp}</p>
+  <p><strong>Name:</strong> ${formData.get("name")}</p>
+  <p><strong>Tanggal Lahir:</strong> ${formData.get("birthday")}</p>
+  <p><strong>Jenis Kelamin:</strong> ${formData.get("gender")}</p>
+  <p><strong>Pesan:</strong> ${formData.get("message")}</p>
+`;
+
+    const receiver = document.getElementById("data-form-receiver");
+    receiver.innerHTML = "";
+    receiver.appendChild(receivedData);
+
+    const bannerSection = document.getElementById("banner-section");
+    const bannerContent = bannerSection.innerHTML;
+    bannerSection.innerHTML = `
+  <h1>Hi ${formData.get("name")}</h1>
+  ${bannerContent}
+  `;
+
+    const placeholder = document.getElementById("placeholder");
+    if (placeholder) {
+      placeholder.remove();
+    }
+  });
